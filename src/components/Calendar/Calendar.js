f170551de2b237e88a106583a6f70c59d0a71084
Schmid Maximilian, GHR-OSA-NEX-31 <Maxi03milian@gmail.com>
2023-01-12 12:07:10 +0100

    changed calendar to logged in user

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -2,11 +2,17 @@ import React from 'react';
 import './Calendar.scss'
 
 function Calendar() {
+
+    let user = JSON.parse(localStorage.getItem('user'));
+    let username = user.username;
+    
+
+
     return (
         <div className='calendar-container'>
         <p>Hello Calendar Here!</p>
         <iframe 
-        src="https://calendar.google.com/calendar/embed?src=maxi03milian%40gmail.com&ctz=Europe%2FZurich"
+        src={`https://calendar.google.com/calendar/embed?src=${username}%40gmail.com&ctz=Europe%2FZurich`}
         style={{ border: 0 }}
         width="800"
         height="600"

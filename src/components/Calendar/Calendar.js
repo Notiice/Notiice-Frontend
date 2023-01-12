d1ee39435545698ca8d74c3d4dfda4987d14adfb
Schmid Maximilian, GHR-OSA-NEX-31 <Maxi03milian@gmail.com>
2023-01-12 11:29:46 +0100

    added basic iframe

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -5,6 +5,14 @@ function Calendar() {
     return (
         <div className='calendar-container'>
         <p>Hello Calendar Here!</p>
+        <iframe 
+        src="https://calendar.google.com/calendar/embed?src=maxi03milian%40gmail.com&ctz=Europe%2FZurich"
+        style={{ border: 0 }}
+        width="800"
+        height="600"
+        frameBorder="0"
+        scrolling="no"
+      />
         </div>
     );
 }
